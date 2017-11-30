0b8a4446a93306f5aad190aad914630fe6242956
Deepak <er.deepakchauhan216@gmail.com>
2017-11-30 23:56:34 +0530

    start react

---
+++ src/config/users.js
@@ -0,0 +1,49 @@
+export default
+{
+    "users": [
+    {
+        "name": "Deepak Kumar"
+    },
+    {
+        "name": "Jayant Chawla"
+    },
+    {
+        "name": "Rachna Kadian"
+    },
+    {
+        "name": "Tanveer Singh"
+    },
+    {
+        "name": "Nitesh Chahuan"
+    },
+    {
+        "name": "Nitesh Kumar Singh"
+    },
+    {
+        "name": "Manpreet Singh"
+    },
+    {
+        "name": "Vikas Kaushik"
+    },
+    {
+        "name": "Tarun Samvedi"
+    },
+    {
+        "name": "Nitin Kaushik"
+    },
+    {
+        "name": "Roshan Yadav"
+    },
+    {
+        "name": "Sakhil Chawla"
+    },
+    {
+        "name": "Jyoti Dhankhar"
+    },
+    {
+        "name": "Swati Sukhija"
+    },
+    {
+        "name": "Isha "
+    }]
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,60 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
+import Users from './config/users.js';
+
+
+class SocialCard extends React.Component {
+	render() {
+		return (
+			<div className="social-card">
+				<div className="social-card-header">
+					<img alt="User " src="/images/user.png"/>
+					<div className="title">
+						<div className="user-name">{this.props.name}</div>
+						<div className="post-time">{this.props.time}</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+
+class SocialApp extends React.Component {
+
+	constructor(props){
+		super(props);
+		this.state = {
+			name : null,
+			time : null,
+			index : 0
+		}
+	}
+
+	setUserData(){	
+		console.log(Users.users);
+		this.setState({name : Users.users[this.state.index].name, time : moment().format("hh:mm a"), index : this.state.index+1});
+	}
+
+	renderCard(){
+		return <SocialCard name={this.state.name} time ={this.state.time}/>
+	}
+	render(){
+		return (
+
+			
+			<div className="social-app">
+				<div onClick={() => this.setUserData()}>
+					Set User Data
+				</div>
+				{this.renderCard()}
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(
+  <SocialApp />,
+  document.getElementById('root')
+);
